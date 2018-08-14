2d23fc770e976a2e921abff54d534d51d3423f76
Marco Arthur <arthurpbs@gmail.com>
2018-08-14 09:26:19 -0300

    Bump version

--- lib/Moose/Meta/Attribute/Custom/Trait/Cache.pm
+++ lib/Moose/Meta/Attribute/Custom/Trait/Cache.pm
@@ -3,7 +3,7 @@ package Moose::Meta::Attribute::Custom::Trait::Cache;
 use Modern::Perl;
 use Moose::Role;
 use Moose::Util 'throw_exception';
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 # time interval in seconds to expire attribute value
 has expiration_time => (
